f88f292fdcd443a4b1dbc4f766ce760870617e85
Braden Carlson <66570644+bradencarlson@users.noreply.github.com>
2021-11-16 12:04:22 -0700

    Update AlgebraicCoding.py
    split lines to improve readability.

--- Python/Cryptography/AlgebraicCoding.py
+++ Python/Cryptography/AlgebraicCoding.py
@@ -3,11 +3,20 @@
 
 # # Algebraic Coding Theory
 
-# SageMath provides a great environment to play with the concepts of Algebraic Coding Theory.  If you have not already, I would highly recommend watching Dr. Andrew Misseldine's [Abstract Algebra](https://www.youtube.com/watch?v=Ox-_-D5bMyk&list=PLz7t89zv8Lp2D6xQOG7kUEbN1KP5u-mpH&index=80) videos on Coding Theory and Binary Symmetric Channels, note that the link leads to the first of several videos on this topic.  
+# SageMath provides a great environment to play with the concepts of Algebraic Coding Theory.  If you have not already, I would
+# highly recommend watching Dr. Andrew Misseldine's 
+# [Abstract Algebra](https://www.youtube.com/watch?v=Ox-_-D5bMyk&list=PLz7t89zv8Lp2D6xQOG7kUEbN1KP5u-mpH&index=80) videos on
+# Coding Theory and Binary Symmetric Channels, note that the link leads to the first of several videos on this topic.  
 # 
-# In SageMath, there are a few new topics we need to be introduced to before we can experiment with Group Codes and encoding messages and transmitting them over a binary symmetric channel.  The first is a new class object called a `MatrixSpace`.  Creating a MatrixSpace will allow us to create matrices and utilize them as our encoding and check matrices for our code.  The second is the command `GF(2)`, which specifies a Field of size 2.  We will use this field for our Matrix Space, since `GF(2)` is implemented as the set {0,1} in SageMath.  
+# In SageMath, there are a few new topics we need to be introduced to before we can experiment with Group Codes and encoding 
+# messages and transmitting them over a binary symmetric channel.  The first is a new class object called a `MatrixSpace`.  
+# Creating a MatrixSpace will allow us to create matrices and utilize them as our encoding and check matrices for our code. 
+# The second is the command `GF(2)`, which specifies a Field of size 2.  We will use this field for our Matrix Space, since 
+# `GF(2)` is implemented as the set {0,1} in SageMath.  
 # 
-# In the code below, we specify two Matrix Spaces, both over `GF(2)`, one of which consists of all 6x3 matrices and the other all 3x6 matrices.  This will give us the tools to define our canonical parity check matrices.  We also define G and H just as Dr. Misseldine defines them in the example he uses in his videos.  
+# In the code below, we specify two Matrix Spaces, both over `GF(2)`, one of which consists of all 6x3 matrices and the other 
+# all 3x6 matrices.  This will give us the tools to define our canonical parity check matrices.  We also define G and H just 
+# as Dr. Misseldine defines them in the example he uses in his videos.  
 
 # In[1]:
 
@@ -25,7 +34,8 @@ print()
 print(H)
 
 
-# Here we define yet another Matrix Space, again over `GF(2)`, this time consisting of all 3x1 matrices, this will be the set of all messages we can encode.  we also define one specific message for us to use.  
+# Here we define yet another Matrix Space, again over `GF(2)`, this time consisting of all 3x1 matrices, this will be the 
+# set of all messages we can encode.  we also define one specific message for us to use.  
 
 # In[2]:
 
@@ -34,7 +44,12 @@ message = MatrixSpace(GF(2),3,1)
 x = message([0,0,1])
 
 
-# Here we define some new classes for our use in this code.  We define the Bernoulli class, which acts as a Bernoulli distribution.  Note that objects of type Bernoulli require a parameter `p`, which will be the probability of success.  We also define the class SymmetricChannel.  This will act as a Binary Symmetric Channel for us to send messages over with some probability of errors.  Each object of type SymmetricChannel needs a Bernoulli distribution to accompany it.  To test whether or not the SymmetricChannel is set up correctly with a Bernoulli distribution, I have provided a function `test()`, which will tell us whether or not our SymmetricChannel is ready to go.  Do not change anything in this cell.
+# Here we define some new classes for our use in this code.  We define the Bernoulli class, which acts as a Bernoulli 
+# distribution.  Note that objects of type Bernoulli require a parameter `p`, which will be the probability of success. 
+# We also define the class SymmetricChannel.  This will act as a Binary Symmetric Channel for us to send messages over 
+# with some probability of errors.  Each object of type SymmetricChannel needs a Bernoulli distribution to accompany it.
+# To test whether or not the SymmetricChannel is set up correctly with a Bernoulli distribution, I have provided a function 
+# `test()`, which will tell us whether or not our SymmetricChannel is ready to go.  Do not change anything in this cell.
 
 # In[1]:
 
@@ -119,7 +134,10 @@ class SymmetricChannel:
         
 
 
-# Here is our first example of sending a message over the channel, we define a Bernoulli distribution, a SymmetricChannel over that Bernoulli distribution, then we test the channel to ensure that it is set up correctly.  Then in the following cell, we define and encode the message, using our encoding message G, then transmit it over the symmetric channel, we can then perform the check with our matrix H, to see if any errors occured in transmitting.
+# Here is our first example of sending a message over the channel, we define a Bernoulli distribution, a SymmetricChannel 
+# over that Bernoulli distribution, then we test the channel to ensure that it is set up correctly.  Then in the following
+# cell, we define and encode the message, using our encoding message G, then transmit it over the symmetric channel, we 
+# can then perform the check with our matrix H, to see if any errors occured in transmitting.
 
 # In[4]:
 
@@ -162,4 +180,6 @@ print(transmitted.transpose())
 print(checked.transpose())
 
 
-# By running the above cell several times, occasionally we should see an error apear in the transmitted message, and the check will not come out to be zero.  Since this is a 1-error correcting code, if only one error occurs, we should see the corresponding column of H print out from the check.  If more errors occur, we must request another transmission.
+# By running the above cell several times, occasionally we should see an error apear in the transmitted message, and the 
+# check will not come out to be zero.  Since this is a 1-error correcting code, if only one error occurs, we should see 
+# the corresponding column of H print out from the check.  If more errors occur, we must request another transmission.
