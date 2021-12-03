fce4920d3ff944aa4409d2e83107ea2938c8a918
Braden Carlson <66570644+bradencarlson@users.noreply.github.com>
2021-12-02 20:21:25 -0700

    Add files via upload

--- Python/Groups/modular-arithmetic.py
+++ Python/Groups/modular-arithmetic.py
@@ -3,16 +3,11 @@
 
 # # The Group of Integers mod *n*
 
-# In this file, we will talk breifly about using Sage to perform modular arithmetic, and then talk about the Integers mod *n* 
-# as a group.  
+# In this file, we will talk breifly about using Sage to perform modular arithmetic, and then talk about the Integers mod *n* as a group.  
 # 
-# The integers mod *n* are implemented as a class called `IntegerModRing` in SageMath.  Hinting at the fact that the integers 
-# mod *n* are in fact a ring, even though maybe at this point we may not be aware of that yet.  Below we define `R` to be the 
-# set of integers mod 12, then we define two elements `a` and `b` of that group and print out the result when they are added together.  
+# The integers mod *n* are implemented as a class called `IntegerModRing` in SageMath.  Hinting at the fact that the integers mod *n* are in fact a ring, even though maybe at this point we may not be aware of that yet.  Below we define `R` to be the set of integers mod 12, then we define two elements `a` and `b` of that group and print out the result when they are added together.  
 # 
-# As we will see in the future, this representation of a group does not offer us a much as we might wish for, such the ability 
-# to print out a Cayley Table, but the integers are simple enough that a computer is not needed to generate the Cayley table for 
-# this group.  
+# As we will see in the future, this representation of a group does not offer us a much as we might wish for, such the ability to print out a Cayley Table, but the integers are simple enough that a computer is not needed to generate the Cayley table for this group.  
 
 # In[ ]:
 
@@ -20,7 +15,6 @@
 R = IntegerModRing(12)
 a = R(11)
 b = R(10)
-print(f"The result of {a} + {b}:")
 print(a+b)
 
 
@@ -28,7 +22,7 @@ print(a+b)
 
 # In[ ]:
 
-print(f"The result of {a} * {b}:")
+
 print(a*b)
 
 
@@ -40,17 +34,14 @@ print(a*b)
 a = R(3)
 b = R(4)
 
-print(f"The result of {a} * {b}:")
 print(a*b)
 
 
-# You will notice that the following code does not do any modular arithmetic, but rather a regular computation on the integers,
-# this is because up untill now we have been declaring $a$ and $b$ as elements of the ring `R`, as so
+# You will notice that the following code does not do any modular arithmetic, but rather a regular computation on the integers, this is because up untill now we have been declaring $a$ and $b$ as elements of the ring `R`, as so
 # 
 # > a = R(3)
 # 
-# When we specify that $a$ needs to belongs to the ring `R`, then SageMath knows to apply the correct binary operations on that
-# element.
+# When we specify that $a$ needs to belongs to the ring `R`, then SageMath knows to apply the correct binary operations on that element.
 
 # In[ ]:
 
@@ -58,13 +49,11 @@ print(a*b)
 # the way we don't want to do this
 a = 5
 b = 7
-print(f"The result of {a} + {b}:")
 print(a+b)
 
 # the way we do want to do this
 a = R(5)
 b = R(7)
-print(f"The result of {a} + {b}:")
 print(a+b)
 
 
@@ -80,9 +69,20 @@ print(a+b)
 # In[ ]:
 
 
-print(f"The inverse of 5 mod 12 is :{inverse_mod(5,12)}")
-print(f"5^1023 mod 45 is : {power_mod(5,1023,45)}")
+print(inverse_mod(5,12))
+print(power_mod(5,1023,45))
 
 
-# For more information about modular arithmetic, visit 
-# [Group Theory and Sage](https://doc.sagemath.org/html/en/thematic_tutorials/group_theory.html#group-theory).
+# For more information about modular arithmetic, visit [Group Theory and Sage](https://doc.sagemath.org/html/en/thematic_tutorials/group_theory.html#group-theory).
+
+# # Modular Arithmetic in Python 
+# 
+# A side note about modular arithmetic in Python and other programming languages outside of SageMath, when dealing with integers, we can perform modular arithmetic with the `%` operator, like so.
+
+# In[ ]:
+
+
+print(1243 % 17)
+
+
+# This computes the remainder of 1243 when divided by 17, or equivalently, 1243 $\text{mod } 17$, We will use this on occasion in this series.  But when performing operations on groups, we will instead use the Ring of Integers from SageMath.
